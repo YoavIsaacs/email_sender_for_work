0478c4927f332a378823335f675ad4ce41478cf6
Yoav Isaacs <yoav.isaacs@gmail.com>
2025-01-17 23:32:52 +0200

    Added station adding

--- main.py
+++ main.py
@@ -55,8 +55,62 @@ def main(stdscr) -> None:
                 location = LOCATIONS[y - len(LOCATIONS) - 2]
 
     stdscr.erase()
-    stdscr.addstr(0, 0, f"You have picked: {location}")
+    stdscr.addstr(0, 0, f"You have picked: {location}. Press any key to start adding stations:")
+    stdscr.getch()
+    stdscr.erase()
     stdscr.refresh()
+
+    finished_adding = False
+    num_of_stations = 0
+    stations = {}
+
+    while not finished_adding:
+        stdscr.addstr(0, 0, f"Adding station #{num_of_stations + 1}:")
+        stdscr.addstr(1, 0, "Please enter the name of the station:")
+
+        text = []
+        cursor_pos = 0
+
+        while True:
+            stdscr.addstr(3, 0, "".join(text) + " ")
+            stdscr.move(3, cursor_pos)
+            curses.curs_set(1)
+            stdscr.refresh()
+
+            key = stdscr.getch()
+
+            if key in (curses.KEY_ENTER, ord("\n"), ord ("\r")):
+                break
+            elif key in (curses.KEY_BACKSPACE, 137):
+                if cursor_pos > 0:
+                    cursor_pos -= 1
+                    del text[cursor_pos]
+            elif key == curses.KEY_LEFT:
+                if cursor_pos > 0:
+                    cursor_pos -= 1
+            elif key == curses.KEY_RIGHT:
+                if cursor_pos < len(text):
+                    cursor_pos += 1
+            elif 32 <= key <= 126:
+                text.insert(cursor_pos, chr(key))
+                cursor_pos += 1
+
+        
+        stations[num_of_stations] = "".join(text)
+        num_of_stations += 1
+
+        stdscr.erase()
+        stdscr.refresh()
+        stdscr.addstr(0, 0, f"You have added station: {stations[num_of_stations - 1]}. To add another station, press enter, to finish press any other key.")
+        stdscr.refresh()
+
+        key = stdscr.getch()
+
+        if key not in (curses.KEY_ENTER, ord("\n"), ord ("\r")):
+            finished_adding = True
+        stdscr.erase()
+        stdscr.refresh()
+
 
 
 
