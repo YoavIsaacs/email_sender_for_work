e5f0b3558f23349f71c5d6f4230e9c4145a47dab
Yoav Isaacs <yoav.isaacs@gmail.com>
2025-01-17 22:08:14 +0200

    printing locations works

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
 import curses
 from curses import wrapper
 
-from utils import EMAILS
+from utils import EMAILS, LOCATIONS
 
 def main(stdscr) -> None:
     stdscr.clear()
     num = 1
-    stdscr.addstr(0, 0, "EMAILS", curses.A_REVERSE)
-    for key in EMAILS:
-        stdscr.addstr(num, 0,"[ ]" + key + ": ")
-        num += 1
-    
+    location = ""
+
+    for loc in LOCATIONS:
+        stdscr.addstr(num, 0, "[ ] " + loc)
+        num +=1
+
+
     stdscr.refresh()
     stdscr.getch()
 
